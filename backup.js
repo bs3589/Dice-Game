89976380ec46e5f0d47b82c0f391ec728dbe41ec
bs3589 <blakesmith3589@gmail.com>
2017-03-08 10:45:09 -0500

    first commit

---
+++ backup.js
@@ -0,0 +1,56 @@
+// // *****Backup 1*****
+
+// var dieRollOne = Math.floor(Math.random() * 6 + 1);
+// document.write("Die one rolled a " + dieRollOne);
+
+// var dieRollTwo = Math.floor(Math.random() * 6 + 1);
+// document.write("Die two rolled a " + dieRollTwo);
+
+// var dieRoleTotal = dieRollOne + dieRollTwo;
+// document.write("Your combined roll is: " + dieRoleTotal);
+
+// var playerTurn = Function (dieRoleTotal); {
+//   if(dieRoleTotal === (7 || 11)) {
+//     alert ("PASS: Next player's turn");
+//   }
+//   else if(dieRoleTotal === (2 || 3 || 12)) {
+//     alert ("CRAP: Your'e shit out of luck!");
+//   }
+//   // else if(dieRoleTotal === 7) {
+//   //   alert ("WINNER: Collect your winnings!");
+//   // }
+//   else {
+//     alert ("Roll again");
+//   }
+//       if (dieRoleTotal ====(4 || 5 || 6 || 8 || 9 || 10)){
+//         dieRoleTotal === point;
+
+//       }
+// }
+
+
+
+// // *****Backup 2*****
+// // Made roll feature work for all numbers
+
+// var dieRollOne = Math.floor(Math.random() * 6 + 1);
+// document.write("Die one rolled a " + dieRollOne);
+
+// var dieRollTwo = Math.floor(Math.random() * 6 + 1);
+// document.write("Die two rolled a " + dieRollTwo);
+
+// var dieRoleTotal = dieRollOne + dieRollTwo;
+// document.write("Your combined roll is: " + dieRoleTotal);
+
+// var playerTurn = Function (dieRoleTotal); 
+//   if(dieRoleTotal === 7 || dieRoleTotal === 11) {
+//     document.write ("PASS: Next player's turn");
+//   }
+//   else if(dieRoleTotal === 2 || dieRoleTotal === 3 || dieRoleTotal === 12) {
+//     document.write ("CRAP: Your'e shit out of luck!");
+//   }
+ 
+//   else if(dieRoleTotal === 4 || dieRoleTotal === 5 || dieRoleTotal === 6 || dieRoleTotal === 8 || dieRoleTotal === 9 || dieRoleTotal === 10){
+//     var point = dieRoleTotal;
+//     document.write("Point is " + point);
+//   }
---
+++ script.js
@@ -0,0 +1,32 @@
+var dieRollOne = Math.floor(Math.random() * 6 + 1);
+document.write("Die one rolled a " + dieRollOne);
+console.log("Die one rolled a " + dieRollOne);
+
+var dieRollTwo = Math.floor(Math.random() * 6 + 1);
+document.write("Die two rolled a " + dieRollTwo);
+console.log("Die two rolled a " + dieRollTwo);
+
+var dieRoleTotal = dieRollOne + dieRollTwo;
+document.write("Your combined roll is: " + dieRoleTotal);
+console.log("Your combined roll is: " + dieRoleTotal);
+
+// button
+document.getElementById("rollButton").onclick = playerTurn;
+
+var playerTurn = Function (dieRoleTotal); {
+  if(dieRoleTotal === 7 || dieRoleTotal === 11) {
+    document.write("PASS: Next player's turn");
+    console.log("PASS: Next player's turn");
+  }
+  else if(dieRoleTotal === 2 || dieRoleTotal === 3 || dieRoleTotal === 12) {
+    document.write("CRAP: Your'e shit out of luck!");
+    console.log("CRAP: Your'e shit out of luck!");
+  }
+ 
+  else if(dieRoleTotal === 4 || dieRoleTotal === 5 || dieRoleTotal === 6 || dieRoleTotal === 8 || dieRoleTotal === 9 || dieRoleTotal === 10){
+    var point = dieRoleTotal;
+    document.write("Point is " + point);
+    console.log("Point is " + point);
+  }
+
+}
